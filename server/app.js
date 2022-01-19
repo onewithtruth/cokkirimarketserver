d1e8b9e617b3ec815555b6a0900cbabe50cfd0a1
Hojin Jang <onewithtruth@gmail.com>
2022-01-19 23:00:50 +0900

    update

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const express = require('express')
 const cors = require('cors');
-const PORT = 443;
+const PORT = 80;
 const fs = require('fs');
 const https = require('https');
 const indexRouter = require('./routes/');
