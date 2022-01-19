7b72bb562d66b1331304807f024964f0c76a6c6e
Hojin Jang <onewithtruth@gmail.com>
2022-01-19 19:56:57 +0900

    update

--- scripts/initialize.sh
+++ scripts/initialize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ubuntu/cokkirimarketserver
+cd /home/ubuntu/cokkirimarketserver/server
 npm install
 npm install pm2@latest -g
 sudo apt-get update
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/cokkirimarketserver
+cd /home/ubuntu/cokkirimarketserver/server
 pm2 stop app.js 2> /dev/null || true
 pm2 delete app.js 2> /dev/null || true
