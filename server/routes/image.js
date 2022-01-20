d20b38c81de210990f5b9d2b2ebd362f4031185d
Hojin Jang <onewithtruth@gmail.com>
2022-01-20 16:10:50 +0900

    Update image.js

--- server/routes/image.js
+++ server/routes/image.js
@@ -36,45 +36,8 @@ const controller = require('../controllers/image')
  *                   example: null
  *                 success:
  *                   type: boolean
- *                   example: true *                 
+ *                   example: true                 
  *
- *       "500":
- *           description: "잘못된 요청입니다."
- *           content:
- *               application/json:
- *                   schema:
- *                       type: object
- *                       properties:
- *                           message:
- *                               type: string
- *                               example: "잘못된 요청입니다."
- * /oauth/google:
- *   post:
- *     description: Google OAuth 2.0 을 통해 가입 및 로그인을 요청합니다.
- *     tags: [OAuth2.0]
- *     produces:
- *     - "application/json"
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:      # Request body contents
- *             type: object
- *             properties:
- *               authorizationCode:
- *                 type: string
- *                 example: <received authorizationCode>
- *     responses:
- *       "201":
- *         description: "성공적으로 로그인 되었습니다."
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "ok"
  *       "500":
  *           description: "잘못된 요청입니다."
  *           content:
