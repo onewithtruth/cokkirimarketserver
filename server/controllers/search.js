8b8ac8afd4ef8d919a896b1c66f738f270829cbb
Hojin Jang <onewithtruth@gmail.com>
2022-01-21 14:10:54 +0900

    Update search.js

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -1,9 +1,45 @@
 const models = require("../models/index");
 const axios = require("axios");
+const { Op } = require("sequelize");
 
 module.exports = {
-  get: (req, res) => {
-      res.status(200).send('GET: /post')
+  get: async (req, res) => {
+    console.log(req.body)  
+    // res.status(200).send('GET: /post')
+    
+    if(req.body.payload){
+      let postLists;
+      let categoryLists;
+      if (req.body.payload.category_id) {
+        categoryLists = await models.post_has_categories.findAll({
+          where: {
+            categories_id: req.body.payload.category_id
+          }
+        })
+        categoryLists = categoryLists.map((elem) => {
+          return elem.dataValues.post_id
+        })
+        console.log(categoryLists)
+        
+        postLists = await models.post.findAll({
+          where: {
+            id: {
+              [Op.or]: categoryLists
+            }
+          }
+        })
+        console.log(postLists[0].dataValues)
+        postLists = postLists.filter((function(elem){
+          return elem.title.indexOf(req.body.payload.query) !== -1;
+        }));
+      }
+
+      res.status(200).send({data: postLists, message: "ok"})
+
+
+    } else {
+        res.status(500).json({ message: '기타 오류' })
+    }
   }
   
   
