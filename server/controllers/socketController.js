be65491c4ba2ae4e2299924d3f614a292949a72c
Hojin Jang <onewithtruth@gmail.com>
2022-01-23 19:01:25 +0900

    Update socketController.js

--- server/controllers/socketController.js
+++ server/controllers/socketController.js
@@ -3,75 +3,75 @@ const { Op } = require("sequelize");
 
 module.exports = {
   get: async (req, res) => {
+    console.log(req.body)
+    res.status(200).send({message: "ok"});
+    // if (req.body.payload.user_id) {
 
-
-    if (req.body.payload.user_id) {
-
-      let myId = req.body.payload.user_id
+    //   let myId = req.body.payload.user_id
   
 
-      let chatIdList = await models.chat.findAll({
-        where: { user_id: myId },
-      });
-      chatIdList = chatIdList.map(function (elem) {
-        return elem.dataValues.id;
-      });
+    //   let chatIdList = await models.chat.findAll({
+    //     where: { user_id: myId },
+    //   });
+    //   chatIdList = chatIdList.map(function (elem) {
+    //     return elem.dataValues.id;
+    //   });
   
 
-      let postIdList = await models.post_has_chat.findAll({
-        where: {
-          chat_id: {
-            [Op.or]: chatIdList,
-          },
-        },
-      });
-      postIdList = postIdList.map(function (elem) {
-        return elem.dataValues.post_id;
-      });
-      let setPostIdList = new Set(postIdList);
-      let uniquePostIdList = [...setPostIdList];
+    //   let postIdList = await models.post_has_chat.findAll({
+    //     where: {
+    //       chat_id: {
+    //         [Op.or]: chatIdList,
+    //       },
+    //     },
+    //   });
+    //   postIdList = postIdList.map(function (elem) {
+    //     return elem.dataValues.post_id;
+    //   });
+    //   let setPostIdList = new Set(postIdList);
+    //   let uniquePostIdList = [...setPostIdList];
       
 
-      let postAuthorIdList = await models.post.findAll({
-        attributes: ["user_id"],
-        where: {
-          id: {
-            [Op.or]: uniquePostIdList,
-          },
-        },
-      });
-      postAuthorIdList = postAuthorIdList.map(function(elem) {
-        return elem.dataValues.user_id;
-      })
+    //   let postAuthorIdList = await models.post.findAll({
+    //     attributes: ["user_id"],
+    //     where: {
+    //       id: {
+    //         [Op.or]: uniquePostIdList,
+    //       },
+    //     },
+    //   });
+    //   postAuthorIdList = postAuthorIdList.map(function(elem) {
+    //     return elem.dataValues.user_id;
+    //   })
     
 
-      let chatListInfo = await models.post.findAll({
-        attributes: ["id", "user_id", "image_src", "title"],
-        where: {
-          id: {
-            [Op.or]: postAuthorIdList,
-          },
-        },
-      })
-      chatListInfo = chatListInfo.map(function(elem) {
-        return {
-          post_id: elem.dataValues.id,
-          user_id: elem.dataValues.user_id,
-          title: elem.dataValues.title,
-          image_src: elem.dataValues.image_src,
-        }
-      })
-      console.log(chatListInfo);
+    //   let chatListInfo = await models.post.findAll({
+    //     attributes: ["id", "user_id", "image_src", "title"],
+    //     where: {
+    //       id: {
+    //         [Op.or]: postAuthorIdList,
+    //       },
+    //     },
+    //   })
+    //   chatListInfo = chatListInfo.map(function(elem) {
+    //     return {
+    //       post_id: elem.dataValues.id,
+    //       user_id: elem.dataValues.user_id,
+    //       title: elem.dataValues.title,
+    //       image_src: elem.dataValues.image_src,
+    //     }
+    //   })
+    //   console.log(chatListInfo);
       
       
   
-      res.status(200).send({data: chatListInfo, message: "ok"});
+    //   res.status(200).send({data: chatListInfo, message: "ok"});
 
-    } else {
+    // } else {
 
-      res.status(400).send({ message: "잘못된 요청입니다." });
+    //   res.status(400).send({ message: "잘못된 요청입니다." });
 
-    }
+    // }
     
   },
 
