72072cb07858abc50fe7346e4dc1d8e8599ef6f1
Hojin Jang <onewithtruth@gmail.com>
2022-01-27 14:43:16 +0900

    Update socket.js

--- server/routes/socket.js
+++ server/routes/socket.js
@@ -9,7 +9,7 @@ const controller = require('../controllers/socketController')
  * 
  * /socket/chatroomlist:
  *   post:
- *     description: DB에 자신의 채팅 리스트를 요청해 받습니다.
+ *     description: DB에 자신의 채팅 Room 리스트를 요청해 받습니다.
  *     tags: [socket.io chat]
  *     produces:
  *     - "application/json"
@@ -46,7 +46,7 @@ const controller = require('../controllers/socketController')
  *                   properties:
  *                     chatListInfoOutput:
  *                       type: "object"
- *                       example: <chatList DATA EXAMPLE>
+ *                       example: <ChatRoomList DATA EXAMPLE>
  *                     "mynickname":
  *                       type: "string"
  *                       example: "c2"
@@ -60,70 +60,46 @@ const controller = require('../controllers/socketController')
  *                           message:
  *                               type: string
  *                               example: "잘못된 요청입니다."
- * /oauth/google:
+ * /socket/chatroom:
  *   post:
- *     description: Google OAuth 2.0 을 통해 가입 및 로그인을 요청합니다.
+ *     description: DB에 자신의 Room의 대화로그 리스트를 요청해 받습니다.
  *     tags: [OAuth2.0]
  *     produces:
  *     - "application/json"
+ *     parameters:
+ *     - in: header
+ *       required: true
+ *       name: User Information
+ *       type: number
+ *       description: UserId
+ *       example: 78
  *     requestBody:
  *       required: true
  *       content:
- *         application/json:
- *           schema:      # Request body contents
- *             type: object
- *             properties:
- *               authorizationCode:
- *                 type: string
- *                 example: <received authorizationCode>
+ *           application/json:
+ *             schema:
+ *               type: "object"
+ *               properties:
+ *                 "payload":
+ *                   type: object
+ *                   properties:
+ *                     "room":
+ *                       type: "string"
+ *                       example: "4#onewithtruth@gmail.com" 
+ *                     "user_id":
+ *                       type: "seller_id"
+ *                       example: 2 
  *     responses:
  *       "201":
- *         description: "성공적으로 로그인 되었습니다."
+ *         description: "ok"
  *         content:
  *           application/json:
  *             schema:
  *               type: object
  *               properties:
- *                 message:
- *                   type: string
- *                   example: "ok"
- *       "500":
- *           description: "잘못된 요청입니다."
- *           content:
- *               application/json:
- *                   schema:
- *                       type: object
- *                       properties:
- *                           message:
- *                               type: string
- *                               example: "잘못된 요청입니다."
- * /oauth/kakao:
- *   post:
- *     description: Kakao OAuth 2.0 을 통해 가입 및 로그인을 요청합니다.
- *     tags: [OAuth2.0]
- *     produces:
- *     - "application/json"
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:      # Request body contents
- *             type: object
- *             properties:
- *               authorizationCode:
- *                 type: string
- *                 example: <received authorizationCode>
- *     responses:
- *       "201":
- *         description: "성공적으로 로그인 되었습니다."
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "ok"
+ *                 data:
+ *                   type: object
+ *                   example: <ChatLogList DATA EXAMPLE>
  *       "500":
  *           description: "잘못된 요청입니다."
  *           content:
